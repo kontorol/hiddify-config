d592b33f75a2bfdcba2b76cf465a3efe09180977
kontorol <60671553+kontorol@users.noreply.github.com>
2022-12-24 18:47:24 -0800

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -5,18 +5,10 @@ mkdir -p ../log
 
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/nginx.conf
+
+ln -s $(pwd)/nginx.conf /etc/nginx/nginx.conf
 
 openssl req -x509 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt -days 3650 -nodes -subj "/C=GB/ST=London/L=London/O=Google Trust Services LLC/CN=www.google.com"
 
-
-sed -i "s/listen 444 ssl;/listen 444 ssl http2;/" web.conf
-
-ln -s $(pwd)/web.conf /etc/nginx/conf.d/web.conf
-mkdir -p /etc/nginx/stream.d/ 
-ln -s $(pwd)/sni-proxy.conf /etc/nginx/stream.d/sni-proxy.conf
-ln -s $(pwd)/signal.conf /etc/nginx/stream.d/signal.conf
-
-if ! grep -Fxq "stream{include /etc/nginx/stream.d/*.conf;}" /etc/nginx/nginx.conf; then
-  echo "stream{include /etc/nginx/stream.d/*.conf;}">>/etc/nginx/nginx.conf;
-fi
 
